b9e20c7ce4df313ab5faba693c17b06413b4d61b
unknown <Administrator@l419.win2k18.local>
2025-01-09 12:52:37 +0530

    s

--- job1.py
+++ job1.py
@@ -4,6 +4,3 @@ for i in range(10):
     ele=random.randint(10,100)
     l.append(ele)
 print("the created list",l)
-print("the sum of list is :",sum(l))
-print("the minium in the list is :",min(l))
-print("the maximum in the list is:",max(l))
